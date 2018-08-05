d94d965e3fdb451b29389a9c09e52a79904cd0ec
nanustek <porvichova.janka@gmail.com>
2018-08-05 10:57:44 +0200

    kalendar first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/janka/Documents/prg/kalendar/django-calendar-master/venv/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
